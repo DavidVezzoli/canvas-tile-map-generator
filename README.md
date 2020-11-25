97d9584658a40f7e62976cd465336acb5ca6d3f0
David Vezzoli <info@davidvezzoli.co.uk>
2020-11-25 22:10:45 +0000

    options added

---
+++ README.md
@@ -0,0 +1,5 @@
+# Canvas rile map array generator
+
+## Project description
+```
+a simple program to use with canvas games with tile maps. Choose map size and tile color and generate a tile map array at the press of a button. 
--- script.js
+++ script.js
@@ -1,21 +1,49 @@
-const wdt = document.getElementById("tileContainer").offsetWidth
-const hgt = document.getElementById("tileContainer").offsetHeight
-const tWdt = 40;
-const tHgt = 40;
+let wdt = 10
+let hgt = 10
+const tWdt = 20;
+const tHgt = 20;
 const tX = wdt / tWdt
 const tY = hgt / tHgt
 
 let outputArray = []
+let clr = "none"
 
-for (let x = 0; x < tX; x++) {
-    for (let y = 0; y < tY; y++) {
-        let tile = document.createElement("div");
-        tile.classList.add("tile")
-        tile.addEventListener('click', () => {
-            tile.classList.toggle("tileOn");
-        })
+document.getElementById("x-tiles").value = wdt
+document.getElementById("y-tiles").value = hgt
 
-        document.getElementById("tileContainer").appendChild(tile)
+document.getElementById("tileContainer").style.width = tWdt * wdt + "px"
+document.getElementById("tileContainer").style.height = tHgt * hgt + "px"
+
+document.getElementById("x-tiles").addEventListener("change", (e) => {
+    document.getElementById("tileContainer").innerHTML = ""
+    wdt = document.getElementById("x-tiles").value
+    newMap()
+})
+
+document.getElementById("y-tiles").addEventListener("change", (e) => {
+    document.getElementById("tileContainer").innerHTML = ""
+    hgt = document.getElementById("y-tiles").value
+    newMap()
+})
+
+document.getElementById("colorSelector").addEventListener("change", (e) => {
+    clr = e.target.value.toLowerCase()
+})
+
+newMap()
+
+function newMap() {
+
+    for (let x = 0; x < wdt; x++) {
+        for (let y = 0; y < hgt; y++) {
+            let tile = document.createElement("div");
+            tile.classList.add("tile")
+            tile.addEventListener('click', () => {
+                tile.style.backgroundColor = clr;
+            })
+
+            document.getElementById("tileContainer").appendChild(tile)
+        }
     }
 }
 
@@ -23,27 +51,33 @@ function generateArray() {
 
     let count = 0;
     outputArray = []
-    for (let y = 0; y < tY; y++) {
+    for (let y = 0; y < hgt; y++) {
         let rowArray = []
-        for (let x = 0; x < tX; x++) {
+        for (let x = 0; x < wdt; x++) {
             let tile = document.getElementById("tileContainer").children[count]
-            if (tile.classList.contains("tileOn")) {
-                rowArray.push(1)
-            } else {
-                rowArray.push(0)
-            }
+            rowArray.push(setTileNumber(tile.style.backgroundColor))
             count++
         }
-
-        let arrayBlock = document.createElement("div")
-        document.getElementById("outputArray").appendChild(arrayBlock)
-        if (y == 0) {
-            arrayBlock.innerHTML += `[[${rowArray}],`
-        } else if (y < tY - 1) {
-            arrayBlock.innerHTML += `[${rowArray}],`
-        } else {
-            arrayBlock.innerHTML += `[${rowArray}]]`
-        }
+        outputArray.push(`<br/>[${rowArray}]`)
     }
+    document.getElementById("outputArray").innerHTML += outputArray
+}
 
+function setTileNumber(tileColor) {
+    switch (tileColor) {
+        case "green":
+            return 1
+        case "brown":
+            return 2
+        case "chocolate":
+            return 3
+        case "red":
+            return 4
+        case "orange":
+            return 5
+        case "purple":
+            return 6
+        default:
+            return 0
+    }
 }
